8a754b66e7076ddc5c8064dbdb8fbddefc2ef36a
maroofgadiwale <108925150+maroofgadiwale@users.noreply.github.com>
2025-03-17 00:22:56 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -70,16 +70,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
+else:
+    raise ValueError("DATABASE_URL is not set in environment variables!")
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
